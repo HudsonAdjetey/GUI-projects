6f7aa9f81ed10b003e3b7587d5f50c6ae7e565ed
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-11 04:11:36 -0700

    updated with create_line

--- Canvas/main.py
+++ Canvas/main.py
@@ -7,7 +7,7 @@ window.geometry("600x400")
 canvas = tk.Canvas(master=window, bg="white")
 canvas.pack()
 
-canvas.create_rectangle((20, 50, 100, 200))
-
+canvas.create_rectangle((100, 50, 100, 200), fill ="red", width=10, dash=(1, 1))
+canvas.create_line(start_x,start_y, end_x, end_y)
 window.mainloop()
 

9c5dedf02d20ea23e0dd956e3ce56a797c432080
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-11 03:54:58 -0700

    update I

--- Canvas/main.py
+++ Canvas/main.py
@@ -7,7 +7,7 @@ window.geometry("600x400")
 canvas = tk.Canvas(master=window, bg="white")
 canvas.pack()
 
-canvas.create_rectangle((0, 0, ))
+canvas.create_rectangle((20, 50, 100, 200))
 
 window.mainloop()
 

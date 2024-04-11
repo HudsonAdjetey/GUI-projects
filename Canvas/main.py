80d2e43846147589ac7fe45916ab89466362e3ae
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-11 07:23:29 -0700

    canva III

--- Canvas/main.py
+++ Canvas/main.py
@@ -8,6 +8,9 @@ canvas = tk.Canvas(master=window, bg="white")
 canvas.pack()
 
 canvas.create_rectangle((100, 50, 100, 200), fill ="red", width=10, dash=(1, 1))
-canvas.create_line(start_x,start_y, end_x, end_y)
+canvas.create_line(10,24, 100, 150)
+canvas.create_oval((0, 100, 100, 200), fill="green")
+canvas.create_polygon((0, 0, 100, 200, 300, 50))
+canvas.create_arc((200, 0, 300, 100))
 window.mainloop()
 
